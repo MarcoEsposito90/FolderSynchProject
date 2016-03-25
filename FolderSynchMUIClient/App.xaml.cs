a99b9ac29ec7df66dab00ae09ef347fbf242679a
MarcoEsposito90 <expolife90@gmail.com>
2016-03-25 19:25:57 +0100

    Creato un nuovo progetto client con interfaccia migliore

---
+++ FolderSynchMUIClient/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FolderSynchMUIClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class FolderSynchApplication : Application
+    {
+        public static FolderSynchService.FolderSynchServiceClient FolderSynchProxy;
+
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+            // first, open connection with the service
+            FolderSynchProxy = new FolderSynchService.FolderSynchServiceClient();
+
+            // opening the main window
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+    }
+}
