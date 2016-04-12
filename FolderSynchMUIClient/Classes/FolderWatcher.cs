ee84ea048780e2fa88bd167148ea22fae82a1304
GiuliaG91 <genta.giulia@gmail.com>
2016-04-12 11:34:38 +0200

    creata struttura dati + watcher funzionante anche per sottocartelle

--- FolderSynchMUIClient/Classes/FolderWatcher.cs
+++ FolderSynchMUIClient/Classes/FolderWatcher.cs
@@ -10,27 +10,31 @@ namespace FolderSynchMUIClient.Classes
     public class FolderWatcher
     {
         FileSystemWatcher watcher;
+        List<string> changedFiles;
 
         public void watch(string folderPath)
         {
             Console.WriteLine("Watcher created for path " + folderPath);
+            changedFiles = new List<string>();
             watcher = new FileSystemWatcher(folderPath);
+            watcher.IncludeSubdirectories = true;
             //watcher.Path = folderPath;
             watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.Size;
             watcher.Changed += OnChanged;
-            watcher.Renamed += OnChanged;
             watcher.Created += OnChanged;
             watcher.Deleted += OnChanged;
+            watcher.Renamed += OnChanged;
             watcher.EnableRaisingEvents = true;
         }
 
         private void OnChanged(object source, FileSystemEventArgs e)
         {
             //capire qui come gestire la creazione degli update 
-
-            Console.WriteLine("OnChanged method called");
+            
             WatcherChangeTypes wct = e.ChangeType;
+            changedFiles.Add(e.FullPath);
             Console.WriteLine("File {0} {1}", e.FullPath, wct.ToString());
+            Console.WriteLine("changedFiles size = {0}", changedFiles.Count);
         }
 
     }
