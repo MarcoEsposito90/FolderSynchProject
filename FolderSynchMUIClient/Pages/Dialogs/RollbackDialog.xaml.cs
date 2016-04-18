d1ca4790180cd96c218e00bfa220b1e5468a97e0
GiuliaG91 <genta.giulia@gmail.com>
2016-04-18 17:34:54 +0200

    grafica rollback

---
+++ FolderSynchMUIClient/Pages/Dialogs/RollbackDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FolderSynchMUIClient
+{
+    /// <summary>
+    /// Interaction logic for RollbackDialog.xaml
+    /// </summary>
+    public partial class RollbackDialog : ModernDialog
+    {
+        public RollbackDialog()
+        {
+            InitializeComponent();
+
+            // define the dialog buttons
+            this.Buttons = new Button[] { this.OkButton, this.CancelButton };
+        }
+
+        private void btnBrowsePath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
