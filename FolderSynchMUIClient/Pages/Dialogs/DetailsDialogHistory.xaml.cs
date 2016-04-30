179d0191328da4567cadc10b24161ca7ac9249fb
MarcoEsposito90 <expolife90@gmail.com>
2016-04-30 17:50:17 +0200

    aggiunto scaricamento file

--- FolderSynchMUIClient/Pages/Dialogs/DetailsDialogHistory.xaml.cs
+++ FolderSynchMUIClient/Pages/Dialogs/DetailsDialogHistory.xaml.cs
@@ -37,7 +37,25 @@ namespace FolderSynchMUIClient
 
         private void downloadButton_Click(object sender, RoutedEventArgs e)
         {
+            // 1) get the update entry ----------------------------
+            Button b = (Button)sender;
+            Update.UpdateEntry entry = (Update.UpdateEntry)b.DataContext;
+            Console.WriteLine("entry = " + entry.ItemLocalPath + "; " + entry.UpdateNumber);
 
+            // 2) compute the folder name -------------------------
+            FileItem fi = (FileItem)this.DataContext;
+            Console.WriteLine("file path = " + fi.Path);
+            string folderPath = fi.Path.Replace("\\" + entry.ItemLocalPath, "");
+            Console.WriteLine("folder Path = " + folderPath);
+
+            // 3) get the LocalFolde3 object ----------------------
+            App application = (App)Application.Current;
+            int index = application.LocalFolders.ToList().FindIndex(item => item.Path.Equals(folderPath));
+            LocalFolder lf = application.LocalFolders.ElementAt(index);
+
+            // 4) display the download dialog ---------------------
+            FileDownloadDialog fdd = new FileDownloadDialog(entry, lf);
+            fdd.ShowDialog();
         }
     }
 }
